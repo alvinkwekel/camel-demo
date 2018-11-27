a98358a79fecc0262e2608cf54208a4cf5f14993
Alvin Kwekel <alvin@liberition.com>
2018-11-27 23:17:47 +0100

    cloudbuild

---
+++ integrations/order/src/main/java/com/liberition/Config.java
@@ -0,0 +1,9 @@
+package com.liberition;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+
+}
--- integrations/order/src/main/java/com/liberition/FileRoute.java
+++ integrations/order/src/main/java/com/liberition/FileRoute.java
@@ -16,5 +16,9 @@ public class FileRoute extends RouteBuilder {
             .transform(method(Transformations.class, "salesOrderToTransportBooking"))
             .marshal().json(JsonLibrary.Jackson)
             .to("log:foo");
+
+        from("timer:foo?delay=3s")
+            .setBody(simple("Hello 1 ${date:now:yyyyMMddhhmmss}"))
+            .to("log:foo");
     }
 }
